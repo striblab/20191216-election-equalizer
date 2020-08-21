76a4a3fe8c48128fbe17c5dd5b20978b31fabbe2
Michael Corey <mikejcorey@gmail.com>
2020-08-21 17:08:09 -0500

    added 3rd party knobs

--- src/App.svelte
+++ src/App.svelte
@@ -23,11 +23,55 @@
 		"urban": 0
 	}
 
+	export let other_party_modifiers = {
+		"rural": 0,
+		"outstate city": 0,
+		"suburban": 0,
+		"urban": 0
+	}
+
+	export let other_party_partisan_modifiers = {
+		"rural": 0.5,
+		"outstate city": 0.5,
+		"suburban": 0.5,
+		"urban": 0.5
+	}
+
 	let projected_r_pct = 0;
 	let projected_d_pct = 0;
 	$ : {
 		projected_r_pct = Math.round(($projected_r_votes_statewide / $projected_total_votes_statewide) * 1000) / 10;
 		projected_d_pct = Math.round(($projected_d_votes_statewide / $projected_total_votes_statewide) * 1000) / 10;
+	}
+
+	let resetDials = function () {
+		partisan_modifiers = {
+			"rural": 0,
+			"outstate city": 0,
+			"suburban": 0,
+			"urban": 0
+		}
+
+		turnout_modifiers = {
+			"rural": 0,
+			"outstate city": 0,
+			"suburban": 0,
+			"urban": 0
+		}
+
+		other_party_modifiers = {
+			"rural": 0,
+			"outstate city": 0,
+			"suburban": 0,
+			"urban": 0
+		}
+
+		other_party_partisan_modifiers = {  // What share of new other votes go to Rs
+			"rural": 0.5,
+			"outstate city": 0.5,
+			"suburban": 0.5,
+			"urban": 0.5
+		}
 	}
 
 </script>
@@ -41,6 +85,10 @@
 			display: inline-block;
 			font-size: 0.7em;
 			margin-right: 2em;
+	}
+
+	.reset-button {
+		margin: 2em 0 1em;
 	}
 
 	.totals {
@@ -71,6 +119,8 @@
 <div class="totals d" class:winner="{$projected_d_votes_statewide > $projected_r_votes_statewide}">Democrat: {projected_d_pct}% {commaNumber($projected_d_votes_statewide)}</div>
 <div>Total statewide projected votes: {commaNumber($projected_total_votes_statewide)}</div>
 
+<button class="reset-button" type="button" on:click={resetDials}>Reset dials</button>
+
 <div id="dashboard">
 	<div id="rural-dashboard">
 		<h3>Rural</h3>
@@ -82,10 +132,20 @@
 			<input type=number bind:value={partisan_modifiers["rural"]} min=-10 max=10 step=0.1 placeholder=0>
 			More Dem <input type=range bind:value={partisan_modifiers["rural"]} min=-10 max=10 step=0.1> More GOP
 		</label>
+
+		<br/><br/>Expert mode:
+		<label>change other party percentage<br/>
+			<input type=number bind:value={other_party_modifiers["rural"]} min=-10 max=10 step=0.1 placeholder=0>
+			<input type=range bind:value={other_party_modifiers["rural"]} min=-10 max=10 step=0.1>
+		</label>
+		<label>Which party do new "other party" votes take from?<br/>
+			<input type=number bind:value={other_party_partisan_modifiers["rural"]} min=0 max=1 step=0.1>
+			All from Dem <input type=range bind:value={other_party_partisan_modifiers["rural"]} min=0 max=1 step=0.1 placeholder=0.5> All from GOP
+		</label>
 	</div>
 
 	<div id="outcity-dashboard">
-		<h3>Outstate city</h3>
+		<h3>Greater MN counties with cities</h3>
 		<label>turnout percentage<br/>
 			<input type=number bind:value={turnout_modifiers["outstate city"]} min=-10 max=10 step=0.1 placeholder=0>
 			<input type=range bind:value={turnout_modifiers["outstate city"]} min=-10 max=10 step=0.1>
@@ -93,6 +153,16 @@
 		<label>partisan balance<br/>
 			<input type=number bind:value={partisan_modifiers["outstate city"]} min=-10 max=10 step=0.1 placeholder=0>
 			More Dem <input type=range bind:value={partisan_modifiers["outstate city"]} min=-10 max=10 step=0.1> More GOP
+		</label>
+
+		<br/><br/>
+		<label>change other party percentage<br/>
+			<input type=number bind:value={other_party_modifiers["outstate city"]} min=-10 max=10 step=0.1 placeholder=0>
+			<input type=range bind:value={other_party_modifiers["outstate city"]} min=-10 max=10 step=0.1>
+		</label>
+		<label>Which party do new "other party" votes take from?<br/>
+			<input type=number bind:value={other_party_partisan_modifiers["outstate city"]} min=0 max=1 step=0.1>
+			All from Dem <input type=range bind:value={other_party_partisan_modifiers["outstate city"]} min=0 max=1 step=0.1 placeholder=0.5> All from GOP
 		</label>
 	</div>
 
@@ -106,10 +176,20 @@
 			<input type=number bind:value={partisan_modifiers["suburban"]} min=-10 max=10 step=0.1 placeholder=0>
 			More Dem <input type=range bind:value={partisan_modifiers["suburban"]} min=-10 max=10 step=0.1> More GOP
 		</label>
+
+		<br/><br/>
+		<label>change other party percentage<br/>
+			<input type=number bind:value={other_party_modifiers["suburban"]} min=-10 max=10 step=0.1 placeholder=0>
+			<input type=range bind:value={other_party_modifiers["suburban"]} min=-10 max=10 step=0.1>
+		</label>
+		<label>Which party do new "other party" votes take from?<br/>
+			<input type=number bind:value={other_party_partisan_modifiers["suburban"]} min=0 max=1 step=0.1>
+			All from Dem <input type=range bind:value={other_party_partisan_modifiers["suburban"]} min=0 max=1 step=0.1 placeholder=0.5> All from GOP
+		</label>
 	</div>
 
 	<div id="urban-dashboard">
-		<h3>Urban</h3>
+		<h3>Hennepin/Ramsey</h3>
 		<label>turnout percentage<br/>
 			<input type=number bind:value={turnout_modifiers["urban"]} min=-10 max=10 step=0.1 placeholder=0>
 			<input type=range bind:value={turnout_modifiers["urban"]} min=-10 max=10 step=0.1>
@@ -117,6 +197,16 @@
 		<label>partisan balance<br/>
 			<input type=number bind:value={partisan_modifiers["urban"]} min=-10 max=10 step=0.1 placeholder=0>
 			More Dem <input type=range bind:value={partisan_modifiers["urban"]} min=-10 max=10 step=0.1> More GOP
+		</label>
+
+		<br/><br/>
+		<label>change other party percentage<br/>
+			<input type=number bind:value={other_party_modifiers["urban"]} min=-10 max=10 step=0.1 placeholder=0>
+			<input type=range bind:value={other_party_modifiers["urban"]} min=-10 max=10 step=0.1>
+		</label>
+		<label>Which party do new "other party" votes take from?<br/>
+			<input type=number bind:value={other_party_partisan_modifiers["urban"]} min=0 max=1 step=0.1>
+			All from Dem <input type=range bind:value={other_party_partisan_modifiers["urban"]} min=0 max=1 step=0.1 placeholder=0.5> All from GOP
 		</label>
 	</div>
 </div>
@@ -126,6 +216,7 @@
 		<th>County</th>
 		<th>Type</th>
 		<th>2016 total votes</th>
+		<th>2016 3rd/other party votes</th>
 		<th>Projected total votes</th>
 		<th>2016 %R</th>
 		<th>2016 %D</th>
@@ -133,6 +224,6 @@
 		<th>Projected %D</th>
 	</tr>
 	{#each county_list as county_data, list_index}
-		<County name={county_data.county} {county_data} bind:turnout_modifier={turnout_modifiers[county_data.classification]} bind:partisan_modifier={partisan_modifiers[county_data.classification]}/>
+		<County name={county_data.county} {county_data} bind:turnout_modifier={turnout_modifiers[county_data.classification]} bind:partisan_modifier={partisan_modifiers[county_data.classification]} bind:other_party_modifier={other_party_modifiers[county_data.classification]} bind:other_party_partisan_modifier={other_party_partisan_modifiers[county_data.classification]}/>
 	{/each}
 </table>
--- src/County.svelte
+++ src/County.svelte
@@ -16,11 +16,25 @@
 	let projected_total_votes = 0;
 	let projected_r_votes = 0;
 	let projected_d_votes = 0;
+	let projected_other_votes = 0;
 	let projected_r_pct = 1;
 	let projected_d_pct = 1;
+	let projected_final_r_pct = 1;
+	let projected_final_d_pct= 1;
+	// let projected_other_pct = 0.5;  // By default each party loses/gains half of projected third party votes
+
+	let thirdPartyCount = function(data_obj) {
+		return data_obj.votes_2016 - data_obj.rVotes_2016 - data_obj.dVotes_2016
+	}
+
+	// let thirdPartyPct = function(data_obj) {
+	// 	return roundPct(thirdPartyCounty / data_obj.votes_2016)
+	// }
 
 	export let turnout_modifier = 0;
 	export let partisan_modifier = 0;
+	export let other_party_modifier = 0;
+	export let other_party_partisan_modifier = 0;
 	$ : {
 		if (!isNaN(partisan_modifier) && !isNaN(turnout_modifier)) {
 			let prev_projected_total_votes = projected_total_votes;
@@ -28,11 +42,20 @@
 			let prev_projected_d_votes = projected_d_votes;
 			projected_total_votes = Math.round(county_data.votes_2016 * (1 + (turnout_modifier / 100)));
 
-			projected_r_pct = county_data.rPct_2016 + (partisan_modifier / 100);
-			projected_d_pct = county_data.dPct_2016 - (partisan_modifier / 100);
+			let projected_base_r_pct = county_data.rPct_2016 + (partisan_modifier / 100);
+			let projected_base_d_pct = county_data.dPct_2016 - (partisan_modifier / 100);
 
-			projected_r_votes = Math.round(projected_r_pct * projected_total_votes);
-			projected_d_votes = Math.round(projected_d_pct * projected_total_votes);
+			projected_other_votes = thirdPartyCount(county_data) * (1 + (other_party_modifier / 100));
+			let projected_other_votes_diff = projected_other_votes - thirdPartyCount(county_data);
+			let projected_other_r_loss = projected_other_votes_diff * other_party_partisan_modifier;
+			let projected_other_d_loss = projected_other_votes_diff * (1 - other_party_partisan_modifier);
+
+
+
+			projected_r_votes = Math.round((projected_base_r_pct * projected_total_votes) - projected_other_r_loss);
+			projected_d_votes = Math.round((projected_base_d_pct * projected_total_votes) - projected_other_d_loss);
+			projected_final_r_pct = projected_r_votes / projected_total_votes;
+			projected_final_d_pct = projected_d_votes / projected_total_votes;
 
 			projected_total_votes_statewide.update(n => n - prev_projected_total_votes + projected_total_votes);
 			projected_r_votes_statewide.update(n => n - prev_projected_r_votes + projected_r_votes);
@@ -68,9 +91,10 @@
 	<td>{name}</td>
 	<td>{county_data.classification}</td>
 	<td class="number">{commaNumber(county_data.votes_2016)}</td>
+	<td class="number">{roundPct(thirdPartyCount(county_data) / county_data.votes_2016)}</td>
 	<td class="number">{commaNumber(projected_total_votes)}</td>
 	<td class="number R-{county_data.winner_2016}-2016">{roundPct(county_data.rPct_2016)}</td>
 	<td class="number D-{county_data.winner_2016}-2016">{roundPct(county_data.dPct_2016)}</td>
-	<td class="number" class:R-R-projected="{projected_r_pct > projected_d_pct}">{roundPct(projected_r_pct)}</td>
-	<td class="number" class:D-D-projected="{projected_r_pct < projected_d_pct}">{roundPct(projected_d_pct)}</td>
+	<td class="number" class:R-R-projected="{projected_final_r_pct > projected_final_d_pct}">{roundPct(projected_final_r_pct)}</td>
+	<td class="number" class:D-D-projected="{projected_final_r_pct < projected_final_d_pct}">{roundPct(projected_final_d_pct)}</td>
 </tr>
